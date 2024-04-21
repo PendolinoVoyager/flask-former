e42c6d909c47c2bd6cc01f93deb3ca775cf7d7ca
PendolinoVoyager <q6894kjz8x34@gmail.com>
2024-04-21 15:34:33 +0200

    landing page done

--- former-frontend/app/page.tsx
+++ former-frontend/app/page.tsx
@@ -1,16 +1,54 @@
+import Image from "next/image";
+import classes from "./page.module.css";
+import mainImage from "@/public/home-main.jpg";
+import securityImage from "@/public/home-security.jpg";
+import connectionsImage from "@/public/home-connections.jpg";
 export default function Home() {
   return (
     <>
-      <h1>TODO</h1>
-      <ul>
-        <li>Main page: home</li>
-        <li>Browse forms + search / filter</li>
-        <li>Form view</li>
-        <li>Form answer</li>
-        <li>Bookmarks</li>
-        <li>Create form</li>
-        <li>View analytics</li>
-      </ul>
+      <h1 className={classes.mainHeader}>Former, just forms.</h1>
+      <section className={classes.sections}>
+        <div className={classes.sectionContent}>
+          <h2>Create Forms</h2>
+          <p>
+            Create custom forms tailored to your needs. <br />
+            Simply drag and drop components that fit your needs.
+          </p>
+        </div>
+        <div className={classes.mainImage}>
+          <Image src={mainImage} alt="Form Creator" placeholder="blur" fill />
+        </div>
+      </section>
+      <section className={classes.sections}>
+        <div className={classes.sectionContent}>
+          <h2>Share Forms</h2>
+          <p>
+            Share your forms with others easily. <br />
+            Copy a link and publish the form wherever you need.
+          </p>
+        </div>
+        <div className={`${classes.mainImage} ${classes.stagger}`}>
+          <Image
+            src={connectionsImage}
+            alt="Form Creator"
+            placeholder="blur"
+            fill
+          />
+        </div>
+      </section>
+      <section className={classes.sections}>
+        <div className={classes.sectionContent}>
+          <h2>Analyze Responses</h2>
+          <p>
+            View and analyze responses to your forms. <br />
+            All responses are anonymous and only accessible <br />
+            to you or people sharing your key.
+          </p>
+        </div>
+        <div className={classes.mainImage}>
+          <Image src={securityImage} alt="Security" placeholder="blur" fill />
+        </div>
+      </section>
     </>
   );
 }
---
+++ former-frontend/components/Footer.tsx
@@ -0,0 +1,17 @@
+export default function Footer() {
+  return (
+    <footer>
+      <p>&copy; 2024 Form Creator</p>
+      <h1>TODO</h1>
+      <ul>
+        <li>Main page: home</li>
+        <li>Browse forms + search / filter</li>
+        <li>Form view</li>
+        <li>Form answer</li>
+        <li>Bookmarks</li>
+        <li>Create form</li>
+        <li>View analytics</li>
+      </ul>
+    </footer>
+  );
+}
