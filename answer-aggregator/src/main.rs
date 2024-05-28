099d7e2d1706d619c68ae6bc35db8e64a70197ff
PendolinoVoyager <q6894kjz8x34@gmail.com>
2024-05-28 20:07:37 +0200

    defined form_analysis schema

--- answer-aggregator/src/main.rs
+++ answer-aggregator/src/main.rs
@@ -1,9 +1,6 @@
-use bson::oid::ObjectId;
-use chrono::Utc;
 use futures::stream::StreamExt;
 use mongodb::{options::ClientOptions, Client};
 use schemas::answer::FormAnswer;
-use schemas::components::{Component, TextComponent};
 use schemas::form::Form;
 
 const DB_URI: &str = "mongodb://127.0.0.1:27017";
@@ -13,22 +10,25 @@ const COLLECTION_ANSWERS: &str = "form_answer";
 const COLLECTION_ANALYSIS: &str = "form_analysis";
 
 mod schemas;
+
 #[tokio::main]
 async fn main() -> mongodb::error::Result<()> {
     let client_uri = DB_URI;
     let mut client_options = ClientOptions::parse(client_uri).await?;
-    client_options.app_name = Some("FormApp".to_string());
+    client_options.app_name = Some("answer_aggregator".to_string());
 
     let client = Client::with_options(client_options)?;
 
     let database = client.database(DB_NAME);
     let forms_collection = database.collection::<Form>(COLLECTION_FORMS);
-    let answers_collection = database.collection::<FormAnswer>(COLLECTION_ANSWERS);
+    let answers_collection: mongodb::Collection<FormAnswer> =
+        database.collection::<FormAnswer>(COLLECTION_ANSWERS);
 
     // Example: Fetch all forms
     let mut cursor = forms_collection.find(None, None).await?;
     while let Some(form) = cursor.next().await {
         println!("{:?}", form);
     }
+
     Ok(())
 }
--- answer-aggregator/src/schemas.rs
+++ answer-aggregator/src/schemas.rs
@@ -11,51 +11,51 @@ pub mod components {
 
     #[derive(Debug, Serialize, Deserialize)]
     pub struct DateComponent {
-        _cls: String,
-        label: String,
-        required: bool,
-        default_value: Option<bson::DateTime>,
+        pub _cls: String,
+        pub label: String,
+        pub required: bool,
+        pub default_value: Option<bson::DateTime>,
     }
 
     #[derive(Debug, Serialize, Deserialize)]
     pub struct NumberComponent {
-        _cls: String,
-        label: String,
-        required: bool,
-        is_integer: bool,
-        default_value: Option<f64>,
+        pub _cls: String,
+        pub label: String,
+        pub required: bool,
+        pub is_integer: bool,
+        pub default_value: Option<f64>,
     }
 
     #[derive(Debug, Serialize, Deserialize)]
     pub struct DateTimeComponent {
-        _cls: String,
-        label: String,
-        required: bool,
-        default_value: Option<bson::DateTime>,
+        pub _cls: String,
+        pub label: String,
+        pub required: bool,
+        pub default_value: Option<bson::DateTime>,
     }
 
     #[derive(Debug, Serialize, Deserialize)]
     pub struct TimeComponent {
-        _cls: String,
-        label: String,
-        required: bool,
-        default_value: Option<String>,
+        pub _cls: String,
+        pub label: String,
+        pub required: bool,
+        pub default_value: Option<String>,
     }
 
     #[derive(Debug, Serialize, Deserialize)]
     pub struct CheckboxComponent {
-        _cls: String,
-        label: String,
-        required: bool,
-        choices: Vec<String>,
+        pub _cls: String,
+        pub label: String,
+        pub required: bool,
+        pub choices: Vec<String>,
     }
 
     #[derive(Debug, Serialize, Deserialize)]
     pub struct RadioComponent {
-        _cls: String,
-        label: String,
-        required: bool,
-        choices: Vec<String>,
+        pub _cls: String,
+        pub label: String,
+        pub required: bool,
+        pub choices: Vec<String>,
     }
 
     #[derive(Debug, Serialize, Deserialize)]
@@ -72,9 +72,11 @@ pub mod components {
 }
 
 pub mod answer {
-    use bson::oid::ObjectId;
+    use bson::{oid::ObjectId, Bson};
 
     use serde::{Deserialize, Serialize};
+
+    use super::{analysis::AnalysisRequest, criteria::Criteria};
 
     #[derive(Debug, Serialize, Deserialize)]
     pub struct FormAnswer {
@@ -82,6 +84,58 @@ pub mod answer {
         pub form: ObjectId,
         pub answers: Vec<bson::Bson>,
         pub submitted_at: bson::DateTime,
+    }
+
+    impl FormAnswer {
+        pub fn analyze(&self, analysis_requests: Vec<AnalysisRequest>) -> Vec<bool> {
+            analysis_requests
+                .iter()
+                .map(|request| {
+                    // Answer is the factual answer. We don't know what it refers to.
+                    // Theres a need to somehow get the hold of ComponentType and
+                    // Implement the method on it.
+                    // The criteria method should return option to prevent invalid criteria
+                    let answer = &self.answers[request.component_index];
+                    match request.criteria {
+                        Criteria::Equals => {
+                            if let Bson::String(ref ans) = answer {
+                                request.equals(ans)
+                            } else {
+                                false
+                            }
+                        }
+                        Criteria::LessThan => {
+                            if let Bson::Double(ans) = answer {
+                                request.less_than(ans)
+                            } else {
+                                false
+                            }
+                        }
+                        Criteria::GreaterThan => {
+                            if let Bson::Double(ans) = answer {
+                                request.greater_than(ans)
+                            } else {
+                                false
+                            }
+                        }
+                        Criteria::StringMatch => {
+                            if let Bson::String(ref ans) = answer {
+                                request.string_match(&Regex::new(ans).unwrap())
+                            } else {
+                                false
+                            }
+                        }
+                        Criteria::OptionChoice => {
+                            if let Bson::Int32(ans) = answer {
+                                request.option_choice(*ans as usize)
+                            } else {
+                                false
+                            }
+                        }
+                    }
+                })
+                .collect()
+        }
     }
 }
 
@@ -99,21 +153,59 @@ pub mod form {
         pub key: Option<String>,
     }
 }
-// Example: Insert a form
-// let form = Form {
-//     _id: ObjectId::new(),
-//     name: "Sample Form".to_string(),
-//     description: "".to_string(),
-//     image: "placeholder.png".to_string(),
-//     created: Utc::now().timestamp(),
-//     components: vec![
-//         Component::Text(TextComponent {
-//             _cls: "TextComponent".to_string(),
-//             label: "New Text".to_string(),
-//             required: true,
-//             default_value: Some("".to_string()),
-//         }),
-//         // Add other components as needed
-//     ],
-//     key: "some_key".to_string(),
-// };
+
+pub mod analysis {
+    use bson::oid::ObjectId;
+    use serde::{Deserialize, Serialize};
+
+    use super::criteria::Criteria;
+
+    #[derive(Debug, Serialize, Deserialize)]
+    pub struct AnalysisRequest {
+        pub criteria: Criteria,
+        pub component_index: usize,
+    }
+    #[derive(Debug, Serialize, Deserialize)]
+    pub struct FormAnalysis {
+        pub _id: ObjectId,
+        pub form: ObjectId,
+        pub answers_included: usize,
+        pub created: i64,
+        pub updated: u64,
+        pub analysis_request: Vec<AnalysisRequest>,
+        pub by_component: Vec<f64>,
+    }
+}
+
+pub mod criteria {
+    use bson::Regex;
+    use serde::{Deserialize, Serialize};
+
+    #[derive(Debug, Serialize, Deserialize)]
+    pub enum Criteria {
+        Equals,
+        LessThan,
+        GreaterThan,
+        StringMatch,
+        OptionChoice,
+    }
+    pub trait Equals {
+        fn equals<T>(&self, value: T) -> bool;
+    }
+
+    pub trait LessThan {
+        fn less_than<T>(&self, value: T) -> bool;
+    }
+
+    pub trait GreaterThan {
+        fn greater_than<T>(&self, value: T) -> bool;
+    }
+
+    pub trait StringMatch {
+        fn string_match(&self, value: &Regex) -> bool;
+    }
+
+    pub trait OptionChoice {
+        fn option_choice(&self, index: usize) -> bool;
+    }
+}
